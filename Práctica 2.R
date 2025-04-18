360710ae680f253fedfe5e3531b42f4563729307
Rocío Hachen <rociohachen@hotmail.com>
2025-04-18 16:39:48 -0300

    Update Final ejercicio 9

--- Práctica 2.R
+++ Práctica 2.R
@@ -99,8 +99,26 @@ for (i in 1:1000){
 plot(1:1000,x, type='l')
 
 
-## Ejercicio 9 - Muestreando de una población finita
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#---------------------------------------------------------------------------
+#                                                                        /  
+#           Ejercicio 9 - Muestreado de una población finita            |
+#                                                                       \ 
+#-------------------------------------------------------------------------
 # 1. Simular el lanzamiento de una moneda equilibrada n = 10 veces. 
 # Contar el número y la proporción de caras obtenidas en la simulación.
 
@@ -108,18 +126,24 @@ plot(1:1000,x, type='l')
 
 # Para la resolución de este ejercicio, definimos que "caras" equivale a 1
 
-# Simulamos la tirada de la moneda 10 veces- 
-muestra = sample(0:1,10,replace=TRUE) # Replace = True permite que haya repetición
+# Simulamos la tirada de la moneda 10 veces
+set.seed(1)
+t = 10
+muestra = sample(0:1,t,replace=TRUE) # Replace = True permite que haya repetición
 
 # Calculamos la proporción de caras obtenidas.
 
 uno_prop = mean(muestra==1)
+cant_uno = sum(muestra==1)
 cero_prop = mean(muestra==0)
+cant_cero = sum(muestra==0)
 #uno = sum(muestra==1)/length(muestra)
 #cero= sum(muestra==0)/length(muestra)
 
-prop = c(uno_prop,cero_prop); prop
+cant = c(cant_uno,cant_cero); cat("Cantidad de 1s:",cant[1],"\nCantidad de 0s:",cant[2])
+prop = c(uno_prop,cero_prop); cat("Proporción de 1s:",prop[1],"\nProporción de 0s:",prop[2])
 
+# ------------------------------------------------------------------------------------------
 # 2. Repetir la simulación para n=1000 y para cada valor desde 1, 2,..., n 
 # calcular y graficar la proporción de caras obtenidas.
 
@@ -130,11 +154,14 @@ x = c() # Inicializamos un vector vacío
 # Además, calculamos la proporción de que la moneda sea 1. Esta proporción
 # Se guarda en el vector x
 
+t = 1000
 for (i in 1:1000){
   
   muestra = sample(0:1,i,replace=TRUE)
-  p = sum(muestra==1)/length(muestra)
-  x <- append(x,p) 
+  # p = sum(muestra==1)/length(muestra)
+  p = mean(muestra==1)
+  x <- append(x,p)
+  
   
 }
 
@@ -142,7 +169,7 @@ for (i in 1:1000){
 
 df <- data.frame(prop = x, len = 1:1000)
 
-ggplot(data = df, aes(x=len, y=prop))+geom_line()+
+ggplot(data = df, aes(x=len, y=prop))+geom_point()+
   
   ggtitle("Plot de proporciones de 1s según número de muestras")+
   ylim(0.0,1.0)+
@@ -150,5 +177,10 @@ ggplot(data = df, aes(x=len, y=prop))+geom_line()+
   labs(x="N° de tiradas por muestra",y="Proporción")+
   geom_hline(yintercept=0.5,linetype="dashed",color="red",size=1)
 
-#plot(1:1000,x, type='l')
+#plot(1:1000,x, type='p')
+
+# Observaciones
+# Se observa el Teorema de los Grandes Números, que establee que la media
+# de los resultados de un experimento aleatorio converge al valor esperado
+# conforme el número de repeticiones aumenta.
 
